d9df4655c61a592955016c0aea4bbb8561b916da
Iñaki Echazarreta <echazarreta90@gmail.com>
2022-05-02 15:36:03 -0300

    Logica de navegacion, Logo pantallas

--- app/src/main/java/com/example/loginscreen/LoginActivity.kt
+++ app/src/main/java/com/example/loginscreen/LoginActivity.kt
@@ -8,8 +8,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
-
-    override fun onStart() {
-        super.onStart()
-    }
 }
